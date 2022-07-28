866209c5805588c5df0a12873d3366b39ee03901
Dragos Mihai <mihaidragosradu@gmail.com>
2022-07-28 20:12:03 +0300

    First commit

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  selectCartCount,
+  selectIsCartOpen,
+} from "../../store/cart/cart.selector.js";
+import { setIsCartOpen } from "../../store/cart/cart.action.js";
+
+import {
+  ShoppingIcon,
+  CartIconContainer,
+  ItemCount,
+} from "./cart-icon.styles.jsx";
+
+const CartIcon = () => {
+  const dispatch = useDispatch();
+  const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className="shopping-icon" />
+      <ItemCount className="item-count">{cartCount}</ItemCount>
+    </CartIconContainer>
+  );
+};
+
+export default CartIcon;
---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { compose, createStore, applyMiddleware } from "redux";
+// import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  next(action);
+};
+
+// const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+//   Boolean
+// );
+const middleWares = [loggerMiddleware];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
